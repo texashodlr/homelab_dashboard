80077c7ac444bd652a09806d2bdec0d48a18fb70
texashodlr <conorxdevlin@gmail.com>
2025-09-14 21:37:46 -0600

    Fixed prome install script

--- prometheus/install.sh
+++ prometheus/install.sh
@@ -1,3 +1,17 @@
+mkdir prometheus && cd prometheus
+
+sleep 1
+
 wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
 
+sleep 1
 
+tar xvfz node_exporter-1.9.1-amd64.tar.gz
+
+sleep 1
+
+cd node_exporter-1.9.1-amd64/
+
+sleep 1
+
+./node_exporter
